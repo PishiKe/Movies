03f167de61bc5b2aa12f379db41b6f28d6404766
PishiKe <nashpeter33@gmail.com>
2022-01-12 11:49:39 +0300

    UI build

--- app/src/main/java/com/pishi/movieappusingjetpackcompose/ui/theme/MovieItem.kt
+++ app/src/main/java/com/pishi/movieappusingjetpackcompose/ui/theme/MovieItem.kt
@@ -1,8 +1,26 @@
 package com.pishi.movieappusingjetpackcompose.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MovieItem(){
-
+    Column(
+        modifier = Modifier
+            .height(200.dp)
+            .width(100.dp)
+            .clip(RoundedCornerShape(32.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f)
+        ) {
+        }
+    }
 }
